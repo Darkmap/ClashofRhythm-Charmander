268b8efdcb39fa007d6901379c7439a379defc82
qixuanwang <darkmap0209@gmail.com>
2016-10-12 21:11:17 -0700

    resources 2/2

---
+++ BattlePrototype/Assets/scripts/MusicUIManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MusicUIManager : MonoBehaviour {
+
+	public GameObject leftRing;
+	public GameObject rightRing;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (MainMusic.play) {
+			if (MainMusic.turnType == 0) {
+				if (!leftRing.activeSelf) {
+					leftRing.SetActive (true);
+				}
+				if (!rightRing.activeSelf) {
+					rightRing.SetActive (true);
+				}
+			} else if (MainMusic.turnType == 1) {
+				if (!leftRing.activeSelf) {
+					leftRing.SetActive (true);
+				}
+				if (rightRing.activeSelf) {
+					rightRing.SetActive (false);
+				}
+			} else{
+				if (leftRing.activeSelf) {
+					leftRing.SetActive (false);
+				}
+				if (!rightRing.activeSelf) {
+					rightRing.SetActive (true);
+				}
+			}
+		}
+	}
+}
